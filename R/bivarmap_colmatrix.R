534d7f1330d97c63c44b2ba54eb3a151ad66decb
seu nome <mauricio.vancine@gmail.com>
2022-02-09 00:22:37 -0300

    0.0.1

---
+++ R/bivarmap_colmatrix.R
@@ -0,0 +1,105 @@
+# The function that produces the colour matrix
+bivarmap_colmatrix <- function(nbreaks = 3,
+                               breakstyle = "quantile",
+                               upperleft = "#0096EB",
+                               upperright = "#820050",
+                               bottomleft = "#BEBEBE",
+                               bottomright = "#FFE60F",
+                               xlab = "x label",
+                               ylab = "y label",
+                               plotLeg = TRUE,
+                               saveLeg = FALSE) {
+
+    usethis::use_package("dplyr")
+    usethis::use_package("ggplot2")
+    usethis::use_package("classInt")
+
+    if (breakstyle == "sd") {
+        warning("SD breaks style cannot be used.\nWill not always return the correct number of breaks.\nSee classInt::classIntervals() for details.\nResetting to quantile",
+                call. = FALSE, immediate. = FALSE)
+        breakstyle <- "quantile"}
+    # The colours can be changed by changing the HEX codes for:
+    # upperleft, upperright, bottomleft, bottomright
+    # From http://www.joshuastevens.net/cartography/make-a-bivariate-choropleth-map/
+    # upperleft = "#64ACBE", upperright = "#574249", bottomleft = "#E8E8E8", bottomright = "#C85A5A",
+    # upperleft = "#BE64AC", upperright = "#3B4994", bottomleft = "#E8E8E8", bottomright = "#5AC8C8",
+    # upperleft = "#73AE80", upperright = "#2A5A5B", bottomleft = "#E8E8E8", bottomright = "#6C83B5",
+    # upperleft = "#9972AF", upperright = "#804D36", bottomleft = "#E8E8E8", bottomright = "#C8B35A",
+    # upperleft = "#DA8DC8", upperright = "#697AA2", bottomleft = "#E8E8E8", bottomright = "#73BCA0",
+    # Similar to Teuling, Stockli, Seneviratnea (2011) [https://doi.org/10.1002/joc.2153]
+    # upperleft = "#F7900A", upperright = "#993A65", bottomleft = "#44B360", bottomright = "#3A88B5",
+    # Viridis style
+    # upperleft = "#FEF287", upperright = "#21908D", bottomleft = "#E8F4F3", bottomright = "#9874A1",
+    # Similar to Fjeldsa, Bowie, Rahbek 2012
+    # upperleft = "#34C21B", upperright = "#FFFFFF", bottomleft = "#595757",  bottomright = "#A874B8",
+    # Default from original source
+    # upperleft = "#0096EB", upperright = "#820050", bottomleft= "#BEBEBE", bottomright = "#FFE60F",
+
+    my.data <- seq(0, 1, .01)
+
+    # Default uses terciles (Lucchesi and Wikle [2017] doi: 10.1002/sta4.150)
+    my.class <- classInt::classIntervals(my.data,
+                                         n = nbreaks,
+                                         style = breakstyle,
+    )
+
+    my.pal.1 <- classInt::findColours(my.class, c(upperleft, bottomleft))
+    my.pal.2 <- classInt::findColours(my.class, c(upperright, bottomright))
+
+    col.matrix <- matrix(nrow = 101, ncol = 101, NA)
+
+    for (i in 1:101) {
+        my.col <- c(paste(my.pal.1[i]), paste(my.pal.2[i]))
+        col.matrix[102 - i, ] <- classInt::findColours(my.class, my.col)
+    }
+
+    ## need to convert this to data.table at some stage.
+    col.matrix.plot <- col.matrix %>%
+        as.data.frame(.) %>%
+        mutate("Y" = row_number()) %>%
+        mutate_at(.tbl = ., .vars = vars(starts_with("V")), .funs = list(as.character)) %>%
+        pivot_longer(data = ., cols = -Y, names_to = "X", values_to = "HEXCode") %>%
+        mutate("X" = as.integer(sub("V", "", .$X))) %>%
+        distinct(as.factor(HEXCode), .keep_all = TRUE) %>%
+        mutate(Y = rev(.$Y)) %>%
+        dplyr::select(-c(4)) %>%
+        mutate("Y" = rep(seq(from = 1, to = nbreaks, by = 1), each = nbreaks),
+               "X" = rep(seq(from = 1, to = nbreaks, by = 1), times = nbreaks)) %>%
+        mutate("UID" = row_number())
+
+    # Use plotLeg if you want a preview of the legend
+    if (plotLeg) {
+        p <- ggplot(col.matrix.plot, aes(X, Y, fill = HEXCode)) +
+            geom_tile() +
+            scale_fill_identity() +
+            coord_equal(expand = FALSE) +
+            theme_void() +
+            theme(aspect.ratio = 1,
+                  axis.title = element_text(size = 12, colour = "black",
+                                            hjust = .5, vjust = 0),
+                  axis.title.y = element_text(angle = 90, hjust = 0.5)) +
+            xlab(bquote(.(xlab) ~  symbol("\256"))) +
+            ylab(bquote(.(ylab) ~  symbol("\256")))
+        print(p)
+        assign(
+            x = "BivLegend",
+            value = p,
+            pos = .GlobalEnv
+        )
+    }
+
+    # Use saveLeg if you want to save a copy of the legend
+    if (saveLeg) {
+        ggsave(filename = "bivLegend.pdf", plot = p, device = "pdf",
+               path = "./", width = 4, height = 4, units = "in",
+               dpi = 300)
+    }
+
+    seqs <- seq(0, 100, (100 / nbreaks))
+    seqs[1] <- 1
+    col.matrix <- col.matrix[c(seqs), c(seqs)]
+    attr(col.matrix, "breakstyle") <- breakstyle
+    attr(col.matrix, "nbreaks") <- nbreaks
+
+    return(col.matrix)
+}
---
+++ R/bivarmap_raster.R
@@ -0,0 +1,75 @@
+# Function to assign colour-codes to raster data
+# As before, by default assign tercile breaks
+bivarmap_raster <- function(rasterx, rastery, colourmatrix = col.matrix,
+                          export.colour.matrix = TRUE,
+                          outname = paste0("colMatrix_rasValues", names(rasterx))) {
+    # TO DO - replace raster with terra #
+    require(raster)
+    require(classInt)
+    # export.colour.matrix will export a data.frame of rastervalues and RGB codes
+    # to the global environment outname defines the name of the data.frame
+    quanx <- getValues(rasterx)
+    tempx <- data.frame(quanx, quantile = rep(NA, length(quanx)))
+    brks <- with(tempx, classIntervals(quanx,
+                                       n = attr(colourmatrix, "nbreaks"),
+                                       style = attr(colourmatrix, "breakstyle"))$brks)
+    ## Add (very) small amount of noise to all but the first break
+    ## https://stackoverflow.com/a/19846365/1710632
+    brks[-1] <- brks[-1] + seq_along(brks[-1]) * .Machine$double.eps
+    r1 <- within(tempx, quantile <- cut(quanx,
+                                        breaks = brks,
+                                        labels = 2:length(brks),
+                                        include.lowest = TRUE))
+    quantr <- data.frame(r1[, 2])
+    quany <- getValues(rastery)
+    tempy <- data.frame(quany, quantile = rep(NA, length(quany)))
+    brksy <- with(tempy, classIntervals(quany,
+                                        n = attr(colourmatrix, "nbreaks"),
+                                        style = attr(colourmatrix, "breakstyle"))$brks)
+    brksy[-1] <- brksy[-1] + seq_along(brksy[-1]) * .Machine$double.eps
+    r2 <- within(tempy, quantile <- cut(quany,
+                                        breaks = brksy,
+                                        labels = 2:length(brksy),
+                                        include.lowest = TRUE
+    ))
+    quantr2 <- data.frame(r2[, 2])
+    as.numeric.factor <- function(x) {
+        as.numeric(levels(x))[x]
+    }
+    col.matrix2 <- colourmatrix
+    cn <- unique(colourmatrix)
+    for (i in 1:length(col.matrix2)) {
+        ifelse(is.na(col.matrix2[i]),
+               col.matrix2[i] <- 1, col.matrix2[i] <- which(
+                   col.matrix2[i] == cn
+               )[1]
+        )
+    }
+    # Export the colour.matrix to data.frame() in the global env
+    # Can then save with write.table() and use in ArcMap/QGIS
+    # Need to save the output raster as integer data-type
+    if (export.colour.matrix) {
+        # create a dataframe of colours corresponding to raster values
+        exportCols <- as.data.frame(cbind(
+            as.vector(col.matrix2), as.vector(colourmatrix),
+            t(col2rgb(as.vector(colourmatrix)))
+        ))
+        # rename columns of data.frame()
+        colnames(exportCols)[1:2] <- c("rasValue", "HEX")
+        # Export to the global environment
+        assign(
+            x = outname,
+            value = exportCols,
+            pos = .GlobalEnv
+        )
+    }
+    cols <- numeric(length(quantr[, 1]))
+    for (i in 1:length(quantr[, 1])) {
+        a <- as.numeric.factor(quantr[i, 1])
+        b <- as.numeric.factor(quantr2[i, 1])
+        cols[i] <- as.numeric(col.matrix2[b, a])
+    }
+    r <- rasterx
+    r[1:length(r)] <- cols
+    return(r)
+}
