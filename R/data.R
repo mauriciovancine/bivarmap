8712009a8ed0e41cef771f34e29a43cef33b999f
Renata Muylaert <renatamuy@gmail.com>
2022-02-15 11:08:02 +1300

    Update data.R

--- R/data.R
+++ R/data.R
@@ -8,12 +8,3 @@
 #'
 #' @source \url{where_does_it_com_from.com}
 "temprec"
-#'
-#' hosts and bias data
-#'
-#' hosts refer to Sarbecovirus host specied estimated by ecological niche models for the present. 
-#' bias refer to sampling rates (residual bias) based on the distance from occurrences to roads, rivers, airports and cities.
-#'
-#' data('TBD')
-#' @source \url{https://www.biorxiv.org/content/10.1101/2021.12.09.471691v1}
-'TBD'
